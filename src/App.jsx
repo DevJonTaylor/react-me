3980e5c471571cb7869e92da3f649574ab6c773d
Jon Taylor <jonnytest1101@icloud.com>
2022-04-13 18:16:44 -0500

    Added a card

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
+import { useEffect } from 'react'
 import logo from './assets/images/logo.svg'
+import card from './assets/images/card.svg'
+import bg from './assets/images/arena.png'
+import { gsap } from 'gsap'
 import './App.css'
 
+
+
+function startGsap() {
+
+  //TweenLite.set('.card',{scaleY:0.8, transformOrigin:'center'});
+
+  gsap.to(".card", 1, {
+      boxShadow: '0 0 25px 25px orange'
+  })
+
+}
+
+const style = {
+  backgroundImage: `url("${bg}")`,
+  backgroundPosition: 'bottom',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  height: '100vh',
+  width: '100%',
+
+}
 function App() {
+  let isUsed = false
+
+  useEffect(() => {
+    if(!isUsed) {
+      isUsed = true
+      startGsap()
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="Jon Taylor's logo" />
-        <p>Work in progress.  Updates coming soon!</p>
+      <header className="App-header" style={ style }>
+        <img alt="my logo" src={logo} />
+        <img alt="project completed" className="card" src={ card } height="250px"/>
       </header>
     </div>
   )
